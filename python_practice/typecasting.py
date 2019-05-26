6ee35860dca5047722a994c7951f2a06bf8fe2a4
shoaib <shoaibraheem9@gmail.com>
2019-05-26 14:33:26 +0500

    Added python_practice

---
+++ python_practice/typecasting.py
@@ -0,0 +1,10 @@
+# Example of type casting
+
+first_name=input("Enter your first name.")
+last_name=input("Enter your last name")
+cust_id=int(input("Enter your id"))
+
+cust_con=(str(cust_id))
+print(first_name+last_name+cust_con)
+
+print(type(cust_id))
