5a9e68288bc7bff17bb9ba2d9b04f4684575b54b
shoaib <shoaibraheem9@gmail.com>
2019-05-28 01:04:09 +0500

    Update dict_practice

--- python_practice/dict_practice.py
+++ python_practice/dict_practice.py
@@ -13,3 +13,30 @@ dict={"shoaib":{"s/o":"rahemm","age":"26"},"waqas":"ismail","noman":"ibrahim"}
 print(dict["shoaib"])
 #if you want to know the value of any nasted key:
 print(dict["shoaib"]["age"])
+
+#to add key and pairs in dictionary:
+dict["danish"]="iqbal"
+print(dict)
+#how to delete any key and pairs in dictionary
+del dict ["danish"]
+print(dict)
+
+#if you want to make a copy of any dictionary: it is used for safe editions.
+dict2=dict.copy()
+del dict2 ["shoaib"]
+print(dict2)
+
+#if you want to get any valeu of any key:
+print(dict.get("shoaib"))
+
+#if you want to update any key value pairs:
+dict2.update({"shoaib":{"s/o":"raheem","age":"26"}})
+print(dict2)
+#if you want to know all key in dictionary:
+print(dict2.keys())
+#if you want to know all keys of your nasted dictionary:
+print(dict2["shoaib"].keys())
+#if you want to know all your items in your dictionary:
+print(dict2.items())
+#if you want to know all your items in your nested dictionary:
+print(dict2["shoaib"].items())
